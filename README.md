19ac2552f35a8d90d9e3ff48bcdb22e383929905
Acreate <1552397900@qq.com>
2026-06-03 17:10:21 +0800

    首次提交代码

---
+++ README.md
@@ -0,0 +1,23 @@
+# WonderlandMusic
+
+#### 介绍
+节点特效软件
+
+#### 软件架构
+qt 6.8.x 以上
+
+# 开发建议
+
+QQ 聊天群 ： 144407981
+
+![MagicRenderView 项目开发群聊二维码](./.assets/MagicRenderView%20%E9%A1%B9%E7%9B%AE%E5%BC%80%E5%8F%91%E7%BE%A4%E8%81%8A%E4%BA%8C%E7%BB%B4%E7%A0%81.png)
+
+# 辅助链接
+
+https://github.com/Acreate/MagicRenderView
+
+https://gitee.com/ChenYLhuman/MagicRenderView
+
+# 赞助
+
+![微信图片_20250603101132](./.assets/funded.png)
---
+++ srcs/cpp/WonderlandMusic/srcs/main.cpp
@@ -0,0 +1,15 @@
+﻿#include <QApplication>
+#include <QPushButton>
+
+int main( int argc, char *argv[ ], char *envp[ ] ) {
+	QApplication application( argc, argv );
+	QPushButton *qPushButton = new QPushButton;
+	qPushButton->setText( QObject::tr( "你好" ) );
+	qPushButton->show( );
+	QObject::connect( qPushButton, &QPushButton::clicked, [&application]( ) {
+		application.quit( );
+	} );
+	int resultExitCode = application.exec( );
+	delete qPushButton;
+	return resultExitCode;
+}

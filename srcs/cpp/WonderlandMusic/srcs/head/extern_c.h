0f217bcf5c0f476a5cee21c57693214d4f9bb367
Acreate <1552397900@qq.com>
2026-09-01 22:46:56 +0800

    链接 ffmpeg 库

---
+++ srcs/cpp/WonderlandMusic/srcs/head/extern_c.h
@@ -0,0 +1,14 @@
+﻿#ifndef EXTERN_C_H_H_HEAD__FILE__
+#define EXTERN_C_H_H_HEAD__FILE__
+
+#ifndef EXTERN_C
+	#ifdef __cplusplus
+		#define  EXTERN_C extern "C"
+	#else
+		#define  EXTERN_C
+	#endif
+#endif
+
+
+
+#endif // EXTERN_C_H_H_HEAD__FILE__
